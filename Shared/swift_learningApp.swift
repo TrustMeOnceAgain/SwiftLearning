2dd9005c6b09f4072b293f1307c6936a612eb42d
Filip Cybuch <filipcybuch@gmail.com>
2022-07-07 13:44:38 +0200

    Initial commit

---
+++ Shared/swift_learningApp.swift
@@ -0,0 +1,20 @@
+//
+//  swift_learningApp.swift
+//  Shared
+//
+//  Created by Filip Cybuch on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_learningApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
