f3039c189f5fec3914f941b11adb1960e95982fe
Filip Cybuch <filipcybuch@gmail.com>
2022-07-15 14:16:30 +0200

    Separated model files

---
+++ Shared/Models/ListColor.swift
@@ -0,0 +1,14 @@
+//
+//  ListColor.swift
+//  swift-learning
+//
+//  Created by Filip Cybuch on 15/07/2022.
+//
+
+import Foundation
+
+struct ListColor {
+    let id: Int
+    let title, userName: String
+    let rgb: ColorModel.RGB
+}
--- Shared/Views/ColorList.swift
+++ Shared/Views/ColorList.swift
@@ -8,64 +8,6 @@
 import SwiftUI
 import Combine
 
-struct ListColor {
-    let id: Int
-    let title, userName: String
-    let rgb: ColorModel.RGB
-}
-
-class ColorListViewModel: ObservableObject {
-    
-    @Published var colors: [ListColor] = []
-    @Published var search: String = ""
-    @Published private var model: [ListColor]?
-    private var cancellable: Set<AnyCancellable> = []
-    private let networkController: ColourLoversRepository
-    
-    init(nController: ColourLoversRepository = DIManager.shared.colourLoversController) {
-        self.networkController = nController
-        setupSearch()
-    }
-    
-    func onAppear() {
-        fetchData()
-    }
-    
-    func onRefreshButtonTap() {
-        fetchData()
-    }
-    
-    private func setupSearch() {
-        $search
-            .combineLatest($model)
-            .map { (search, model) in
-                guard !search.isEmpty else { return model }
-                return model?.filter { $0.title.contains(search) }
-            }
-            .sink(receiveValue: { [weak self] in
-                self?.colors = $0 ?? []
-            })
-            .store(in: &cancellable)
-    }
-    
-    private func fetchData() {
-        networkController.getColorsCombine()
-            .map { models in
-                models.map {
-                    ListColor(id: $0.id, title: $0.title, userName: $0.userName, rgb: $0.rgb)
-                }
-            }
-            .sink(receiveCompletion: { completion in
-                print("\(#function): \(completion)")
-            }, receiveValue: { [weak self] model in
-                print("New Value!")
-                self?.model = model
-                
-            })
-            .store(in: &cancellable)
-    }
-}
-
 struct ColorList: View {
     
     private let networkController = DIManager.shared.colourLoversController
@@ -77,7 +19,7 @@ struct ColorList: View {
     
     var body: some View {
         NavigationView {
-            createView(baseOn: viewModel.colors)
+            createView()
                 .frame(minWidth: 300)
         }
         .navigationTitle("ColourLovers")
@@ -88,14 +30,15 @@ struct ColorList: View {
 // MARK: View builders
 extension ColorList {
     @ViewBuilder
-    private func createView(baseOn model: [ListColor]) -> some View {
+    private func createView() -> some View {
         VStack {
             TextField("Search", text: $viewModel.search)
-            if model.isEmpty {
-                createInfoView()
+            if viewModel.colors.isEmpty {
+                InfoView(onAppearAction: { if viewModel.search == "" { viewModel.onAppear() } },
+                         onButtonTapAction: { viewModel.onRefreshButtonTap() })
             } else {
                 List {
-                    createColorCells(model: model)
+                    createColorCells(model: viewModel.colors)
                 }
                 .listStyle(.inset)
             }
@@ -109,18 +52,29 @@ extension ColorList {
             }
         }
     }
+}
+
+struct InfoView: View {
     
-    private func createInfoView() -> some View {
+    let onAppearAction: (() -> ())?
+    let onButtonTapAction: (() -> ())?
+    
+    init(onAppearAction: (() -> ())?, onButtonTapAction: (() -> ())?) {
+        self.onAppearAction = onAppearAction
+        self.onButtonTapAction = onButtonTapAction
+    }
+    
+    var body: some View {
         VStack(alignment: .center) {
             Spacer()
             Text("There are no colours to show!")
             Button("Refresh list") {
-                viewModel.onRefreshButtonTap()
+                onButtonTapAction?()
             }
             Spacer()
         }
         .onAppear {
-            viewModel.onAppear()
+            onAppearAction?()
         }
     }
 }
